3a5eecb9a28f4f946d310feaeb952360d4672e86
Mark Crowder <crowdered@gmail.com>
2019-03-22 12:31:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
- i = 0
-collect = []
-while i > array.length
-collect << yield(array[1])
-i += 1
-end
-collect
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
